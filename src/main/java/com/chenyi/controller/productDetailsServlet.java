2376076f8e7cddd67446262fde4a904ec2c59d87
cheyi-29 <1261599503@qq.com>
2021-05-19 17:44:34 +0800

    HW-Week 12 Commit

--- src/main/java/com/chenyi/controller/productDetailsServlet.java
+++ src/main/java/com/chenyi/controller/productDetailsServlet.java
@@ -2,6 +2,7 @@ package com.chenyi.controller;
 
 import com.chenyi.dao.IProductDao;
 import com.chenyi.dao.ProductDao;
+import com.chenyi.model.Category;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -11,25 +12,30 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
-@WebServlet(name = "ProductListServlet", value = "/admin/ProductList")
-public class ProductListServlet extends HttpServlet {
+@WebServlet(name = "productDetailsServlet", value = "/productDetails")
+public class productDetailsServlet extends HttpServlet {
     Connection con=null;
+
     @Override
-    public  void  init() throws ServletException{
+    public void init() throws ServletException {
         super.init();
         con=(Connection) getServletContext().getAttribute("con");
     }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ProductDao productDao=new ProductDao();
         try {
-            List<IProductDao.Product> productList=productDao.findAll(con);
-            request.setAttribute("productList",productList);
+            List<Category> categoryList=Category.findAllCategory(con);
+            request.setAttribute("categoryList",categoryList);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-        String path="WEB-INF/views/admin/productList.jsp";
-        request.getRequestDispatcher(path).forward(request,response);
+        if (request.getParameter("id")!=null){
+            int productId= Integer.parseInt(request.getParameter("id"));
+            ProductDao productDao=new ProductDao();
+            IProductDao.Product product=productDao.findById(productId,con);
+            request.setAttribute("p",product);
+        }
 
     }
 
